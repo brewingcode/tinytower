bf3787ab3f43b47c3e3c5cf3220a6054157639b1
Alex <aperlscript@gmail.com>
2014-05-26 10:39:37 -0700

    add more to tables:
    - each user has a "quest", their current active quest
    - each floor has a "category"
    - all residential floors, plus the Lobby, are category "none"

--- src/tables.sql
+++ src/tables.sql
@@ -1,9 +1,11 @@
 drop table if exists users;
-create table if not exists users (id integer primary key, name string);
+create table if not exists users (id integer primary key, name string, quest integer);
 
 drop table if exists floors;
-create table if not exists floors (id integer primary key, name string);
-insert into floors(id, name) values (1, "Lobby");
+create table if not exists floors (id integer primary key, name string, category string);
+insert into floors(id, name, category) values
+  (1, "Lobby", "none"),
+  (2, "Residential", "none");
 
 drop table if exists towers;
 create table if not exists towers (user integer, floor integer, story integer);
