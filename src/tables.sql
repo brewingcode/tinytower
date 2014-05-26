58c527276a06b48aa6db5a9b32075d249ed099ed
Alex <aperlscript@gmail.com>
2014-05-26 09:43:54 -0700

    add Lobby as pre-existing floor

--- src/tables.sql
+++ src/tables.sql
@@ -3,6 +3,7 @@ create table if not exists users (id integer primary key, name string);
 
 drop table if exists floors;
 create table if not exists floors (id integer primary key, name string);
+insert into floors(id, name) values (1, "Lobby");
 
 drop table if exists towers;
 create table if not exists towers (user integer, floor integer, story integer);
